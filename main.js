6ad28601e5869bda7d8c48a716805acee8558c69
Ism1tha <xgalan@hotmail.com>
2023-05-09 19:26:44 +0200

    3. Attribute Binding

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Buggati Veyron',
-            product_description: 'The fastest car in the world',
+            currentProductIndex: 1,
+            products: {
+                0: {
+                    name: 'Socks Blue',
+                    image: 'assets/images/socks_blue.jpg',
+                    link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks+blue',
+                    inventory: 10
+                },
+                1: {
+                    name: 'Socks Green',
+                    image: 'assets/images/socks_green.jpg',
+                    link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks+green',
+                    inventory: 0
+                },
+            }
         }
     }
 })
