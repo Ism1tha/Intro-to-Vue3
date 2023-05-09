cf75aa7475abe546e7bd703c5552ea5b4a89c486
Ism1tha <xgalan@hotmail.com>
2023-05-09 19:07:03 +0200

    2. Creating the Vue App

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Buggati Veyron',
+            product_description: 'The fastest car in the world',
+        }
+    }
+})
