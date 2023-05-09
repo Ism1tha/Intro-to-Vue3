71252930ba5b734b591c3bf56c464d410fabbc59
Ism1tha <xgalan@hotmail.com>
2023-05-09 19:43:38 +0200

    5. List Rendering

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 const app = Vue.createApp({
     data() {
         return {
-            currentProductIndex: 0,
-            products: {
+            currentVariantIndex: 0,
+            variants: {
                 0: {
                     name: 'Socks Blue',
                     image: 'assets/images/socks_blue.jpg',
                     link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks+blue',
                     inventory: 10,
                     onSale: true,
+                    details: ['50% cotton', '30% wool', '20% polyester'],
+                    sizes: ['S', 'M', 'L', 'XL'],
                 },
                 1: {
                     name: 'Socks Green',
@@ -16,6 +18,8 @@ const app = Vue.createApp({
                     link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks+green',
                     inventory: 0,
                     onSale: false,
+                    details: ['50% cotton', '30% wool', '20% polyester'],
+                    sizes: ['S', 'M', 'L', 'XL'],
                 },
             }
         }
