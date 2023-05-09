44f8972b2fb45e3bf226168b80fe47bbcdaa70cc
Ism1tha <xgalan@hotmail.com>
2023-05-09 19:33:30 +0200

    4.0 Conditional Rendering

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            currentProductIndex: 1,
+            currentProductIndex: 0,
             products: {
                 0: {
                     name: 'Socks Blue',
                     image: 'assets/images/socks_blue.jpg',
                     link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks+blue',
-                    inventory: 10
+                    inventory: 10,
+                    onSale: true,
                 },
                 1: {
                     name: 'Socks Green',
                     image: 'assets/images/socks_green.jpg',
                     link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks+green',
-                    inventory: 0
+                    inventory: 0,
+                    onSale: false,
                 },
             }
         }
